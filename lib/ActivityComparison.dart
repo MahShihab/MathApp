94acbb2d51a7b7c127e074aed4f5718c070ca73e
Mahmoud <sandi.pc.man@gmail.com>
2023-03-26 15:41:35 +0300

    first activity

--- lib/ActivityComparison.dart
+++ lib/ActivityComparison.dart
@@ -1,15 +1,6 @@
 import 'dart:js';
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:project/Data.dart';
-import 'package:project/StudentInfo.dart';
-import 'package:project/main.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:project/profile.dart';
-import 'AdminHome.dart';
-import 'firebase_options.dart';
 
 class ACompar extends StatefulWidget {
   const ACompar({Key? key}) : super(key: key);
@@ -19,8 +10,10 @@ class ACompar extends StatefulWidget {
 }
 
 class _AComparState extends State<ACompar> {
-  late int correctIndex;
   List<String> options = ['Option 1', 'Option 2', 'Option 3'];
+  late int correctIndex = Random()
+      .nextInt(options.length - 1); // subtract 1 to exclude the empty string
+  int? droppedIndex;
 
   @override
   void initState() {
@@ -34,88 +27,105 @@ class _AComparState extends State<ACompar> {
     return Scaffold(
       backgroundColor: const Color(0xfff4f4f4),
       body: Center(
-        child: Column(children: [
-          const SizedBox(height: 40),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // The draggable containers
-              for (int i = 0; i < options.length; i++)
-                Draggable(
-                  data: i,
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                    child: Center(
-                      child: Text(
-                        options[i],
-                        style: TextStyle(color: Colors.white),
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // The draggable containers
+                for (int i = 0; i < options.length; i++)
+                  Draggable(
+                    data: i,
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.blue,
+                      child: Center(
+                        child: Text(
+                          options[i],
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    feedback: Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.blue,
+                      child: Center(
+                        child: Text(
+                          options[i],
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ),
                     ),
                   ),
-                  feedback: Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue.withOpacity(0.5),
-                    child: Center(
-                      child: Text(
-                        options[i],
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-              const SizedBox(width: 20),
-              // The drop target
-            ],
-          ),
-          const SizedBox(height: 40),
-          SizedBox(
-            height: 100,
-            width: 200,
-            child: DragTarget<int>(
-              builder: (BuildContext context, List<int?> candidateData,
-                  List<dynamic> rejectedData) {
-                // The drop target widget
-                return Container(
+                // const SizedBox(width: 20),
+                // The drop target or dropped container
+              ],
+            ),
+            const SizedBox(height: 40),
+            Stack(
+              children: [
+                
+                Container(
+                  width: 100,
+                  height: 100,
                   decoration: BoxDecoration(
                     border: Border.all(width: 2),
                     borderRadius: BorderRadius.circular(10),
                   ),
                   child: Center(
-                    child: Text(
-                      'Drag and drop the correct answer here',
-                      textAlign: TextAlign.center,
-                    ),
+                    child: droppedIndex != null
+                        ? Text(
+                            options[droppedIndex!],
+                            style: TextStyle(color: Colors.black),
+                          )
+                        : Text(
+                            'Drag and drop the correct answer here',
+                            textAlign: TextAlign.center,
+                          ),
                   ),
-                );
-              },
-              onWillAccept: (data) => true,
-              onAccept: (data) {
-                // Check if the dragged container is the correct answer
-                if (data == correctIndex) {
-                  // Get the current position of the drop target
-                  int dropTargetIndex = options.indexOf('');
-
-                  // Swap the correct option with the drop target
-                  setState(() {
-                    options[dropTargetIndex] = 'Option ${correctIndex + 1}';
-                    options[correctIndex] = '';
-                  });
-
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Correct!')),
-                  );
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Incorrect!')),
-                  );
-                }
-              },
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: DragTarget<int>(
+                    builder: (BuildContext context, List<int?> candidateData,
+                        List<dynamic> rejectedData) {
+                      // The invisible drop target widget
+                      return Container(decoration: BoxDecoration(
+                    border: Border.all(width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),);
+                    },
+                    onWillAccept: (data) => true,
+                    onAccept: (data) {
+                      // Check if the dragged container is the correct answer
+                      if (data == correctIndex) {
+                        // Check if the drop target is empty
+                        if (droppedIndex == null) {
+                          setState(() {
+                            // Replace the drop target with the dropped container
+                            droppedIndex = data;
+                            options[data] = options[correctIndex];
+                          });
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Correct!')),
+                          );
+                        }
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Incorrect!')),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),
-        ]),
+          ],
+        ),
       ),
     );
   }
